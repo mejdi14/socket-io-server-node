7622f172841dbd68275de0e301b4b1dd26abb92a
mejdi14 <mejdiclubiste@gmail.com>
2021-03-15 15:08:05 +0100

    change the socket completly

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-const io = require('socket.io')(http);
+const server = require('http').createServer()
+const io = require('socket.io')(server)
 
-app.get('/', (req, res) => {
-    res.send("Node Server is running. Yay!!")
+io.on('connection', function (client) {
+
+  console.log('client connect...', client.id);
+
+  client.on('typing', function name(data) {
+    console.log(data);
+    io.emit('typing', data)
+  })
+
+  client.on('message', function name(data) {
+    console.log(data);
+    io.emit('message', data)
+  })
+
+  client.on('location', function name(data) {
+    console.log(data);
+    io.emit('location', data);
+  })
+
+  client.on('connect', function () {
+  })
+
+  client.on('disconnect', function () {
+    console.log('client disconnect...', client.id)
+    // handleDisconnect()
+  })
+
+  client.on('error', function (err) {
+    console.log('received error from client:', client.id)
+    console.log(err)
+  })
 })
 
-io.on('connection', socket => {
-    //Get the chatID of the user and join in a room of the same chatID
-    chatID = socket.handshake.query.chatID
-    socket.join(chatID)
-    console.log(`this is me ${chatID}`)
-
-    //Leave the room if the user closes the socket
-    socket.on('disconnect', () => {
-        socket.leave(chatID)
-    })
-
-    //Send message to only a particular user
-    socket.on('send_message', message => {
-        receiverChatID = message.receiverChatID
-        senderChatID = message.senderChatID
-        content = message.content
-
-        //Send message to only that particular room
-        socket.in(receiverChatID).emit('receive_message', {
-            'content': content,
-            'senderChatID': senderChatID,
-            'receiverChatID':receiverChatID,
-        })
-    })
+var server_port = process.env.PORT || 3000;
+server.listen(server_port, function (err) {
+  if (err) throw err
+  console.log('Listening on port %d', server_port);
 });
-
-http.listen(8080)
